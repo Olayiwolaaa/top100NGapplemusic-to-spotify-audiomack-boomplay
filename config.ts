f3b3a563ce7c84ae64a935f9b123b4a40f5a3747
Olayiwolaaa <muizzkara91@gmail.com>
2024-01-29 19:00:35 +0100

    dir structuring

---
+++ config.ts
@@ -0,0 +1,9 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const NG_APPLE_MUSIC_URL = process.env.NG_APPLE_MUSIC_URL;
+// export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+// export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+// export const SPOTIFY_ACCESS_TOKEN = process.env.SPOTIFY_ACCESS_TOKEN;
+// export const SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN;
---
+++ src/applemusic-scraper.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import cheerio from "cheerio";
+// import { NG_APPLE_MUSIC_URL } from "./config";
+
+async function scrapeAppleMusicPlaylist(url: string) {
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+
+    // Example: Extract song names
+    const songNames = $(".song-name")
+      .map((index, element) => $(element).text())
+      .get();
+
+    return songNames;
+  } catch (error) {
+    console.error("Error scraping playlist:", error);
+    throw error;
+  }
+}
+
+
+const playlistUrl = "https://music.apple.com/vg/playlist/top-100-nigeria/pl.2fc68f6d68004ae993dadfe99de83877";
+scrapeAppleMusicPlaylist(playlistUrl)
+  .then((songNames) => {
+    console.log("Song names:", songNames);
+    // Process scraped data as needed
+  })
+  .catch((error) => {
+    console.error("Error scraping playlist:", error);
+  });
